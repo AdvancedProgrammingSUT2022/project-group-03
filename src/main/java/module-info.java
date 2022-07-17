96231db40b22f8b3a1f7adf2c93fcd534dbd5508
Sayyed Ali Tayyeb <salitayyeb@gmail.com>
2022-07-17 13:36:19 +0430

    add the JCommander to the project again

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,15 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
-//    requires javafx.media;
     requires com.google.gson;
-
+    requires jcommander;
 
     opens com.example.demo;
     exports com.example.demo;
-    exports com.example.demo.controller;
-    exports com.example.demo.model;
     opens com.example.demo.controller;
+    exports com.example.demo.controller;
     opens com.example.demo.model;
-
+    exports com.example.demo.model;
     opens com.example.demo.view;
     exports com.example.demo.view;
 }
