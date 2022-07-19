1cc31cfe499941409e752e1b128df389762a6d15
Sayyed Ali Tayyeb <salitayyeb@gmail.com>
2022-07-19 12:02:10 +0430

    fix the project structure

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,7 +9,7 @@ module com.example.demo {
     opens com.example.demo.controller;
     exports com.example.demo.controller;
     opens com.example.demo.model;
-    opens com.example.demo.font;
+//    opens com.example.demo.font;
     exports com.example.demo.model;
     opens com.example.demo.view;
     exports com.example.demo.view;
