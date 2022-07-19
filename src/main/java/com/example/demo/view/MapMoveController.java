8207c213e456cdba563616e60d366de5db4991eb
Sayyed Ali Tayyeb <salitayyeb@gmail.com>
2022-07-19 15:50:49 +0430

    feature: Add showTile to MapMoveController

--- src/main/java/com/example/demo/view/MapMoveController.java
+++ src/main/java/com/example/demo/view/MapMoveController.java
@@ -1,5 +1,7 @@
 package com.example.demo.view;
 
+import com.example.demo.model.tiles.Tile;
+import com.example.demo.view.model.GraphicTile;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.Pane;
@@ -12,8 +14,10 @@ public class MapMoveController {
     private double minY;
     private double maxX;
     private double maxY;
+    private static Pane upperMapPane;
 
-    public MapMoveController(Pane root, Pane upperMapPane,double minX, double maxX, double minY, double maxY, boolean changeY,boolean doZoom) {
+    public MapMoveController(Pane root, Pane upperMapPane, double minX, double maxX, double minY, double maxY, boolean changeY, boolean doZoom) {
+        MapMoveController.upperMapPane = upperMapPane;
         this.minX = minX;
         this.minY = minY;
         this.maxX = maxX;
@@ -27,15 +31,14 @@ public class MapMoveController {
             double x = mouseEvent.getScreenX() + startX;
             double y = mouseEvent.getScreenY() + startY;
 //            if (x < 500 && x > -mapPane.getWidth() + 700)
-            if(x>minX && x<maxX)
-            upperMapPane.setTranslateX(x);
+            if (x > minX && x < maxX)
+                upperMapPane.setTranslateX(x);
 //            if (y < 500 && y > -mapPane.getHeight() + 500)
-            if(y>minY && y<maxY && changeY)
-            upperMapPane.setTranslateY(y);
+            if (y > minY && y < maxY && changeY)
+                upperMapPane.setTranslateY(y);
         });
 
-        if(doZoom)
-        {
+        if (doZoom) {
             //zoom in/out on map
             upperMapPane.setPrefHeight(Screen.getPrimary().getBounds().getHeight());
             upperMapPane.setPrefWidth(Screen.getPrimary().getBounds().getWidth());
@@ -53,6 +56,13 @@ public class MapMoveController {
                 upperMapPane.setTranslateY(upperMapPane.getTranslateY() * translateScale);
             });
         }
+    }
+
+    public static void showTile(GraphicTile tile) {
+        upperMapPane.setScaleX(1);
+        upperMapPane.setScaleY(1);
+        upperMapPane.setTranslateX(-tile.getX() + 340);
+        upperMapPane.setTranslateY(-tile.getY() + 270);
     }
 
     public double getStartX() {
