2a10a37262725b39897500752c949a6eebdb839b
aryatrb <49511934+aryatrb@users.noreply.github.com>
2022-07-28 11:47:20 +0430

    checkout branch commit

--- Client/src/main/java/com/example/demo/view/model/GraphicTile.java
+++ Client/src/main/java/com/example/demo/view/model/GraphicTile.java
@@ -237,7 +237,7 @@ public class GraphicTile implements Serializable {
                 addButton("Attack", true, false, event1 -> {
                     int x = GameController.getSelectedTile().getX();
                     int y = GameController.getSelectedTile().getY();
-                    String code =NetworkController.send("unitAttack "+ tile.getX()+" "+ tile.getY()+" "+
+                    String code =NetworkController.send("unitAttack "+ x+" "+ y+" "+
                             unitToString(GameController.getSelectedUnit()));
                     if(code.length()>1) {
                         Alert alert = StageController.errorMaker("Declaring war, eh?", "By this action, you will declare war to " +
